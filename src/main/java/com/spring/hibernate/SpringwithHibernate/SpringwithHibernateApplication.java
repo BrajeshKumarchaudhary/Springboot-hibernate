d6f39bdea7f92f140191cd836822e16a15516adc
brajesh <brajesh.kumar@squareyards.co.in>
2020-02-24 10:16:26 +0530

    initial commit

---
+++ src/main/java/com/spring/hibernate/SpringwithHibernate/SpringwithHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.spring.hibernate.SpringwithHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwithHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwithHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/hibernate/SpringwithHibernate/SpringwithHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.hibernate.SpringwithHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwithHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
